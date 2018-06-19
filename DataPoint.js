760b66236d509d303a3f369ad34ac7a49a3dad12
Jonah Rubin <jonah.n.rubin@gmail.com>
2018-06-19 18:11:13 -0400

    Adding local + UTC timestamp

--- DataPoint.js
+++ DataPoint.js
@@ -1,24 +1,31 @@
+/**
+	DataPoint object class
+
+	@Author Jonah Rubin 
+	5/31/18
+*/
+
 // create datapoint object
 function DataPoint(name, stdt, stla, stlo, hdop, vdop, Vbat, minV, Pint, Pext, Prange, cmdrdc, f2up, fupl) {
 	this.name = name;
 	this.stdt = stdt;
-	this.loct = toDate(stdt);
-        this.stla = stla;
-        this.stlo = stlo;
-        this.hdop = hdop;
+	this.loct = toLocDate(stdt);
+  this.stla = stla;
+  this.stlo = stlo;
+	this.hdop = hdop;
 	this.vdop = vdop;
 	this.Vbat = Vbat;
 	this.minV = minV;
 	this.Pint = Pint;
 	this.Pext = Pext;
 	this.Prange = Prange;
-        this.cmdrdc = cmdrdc;
-        this.f2up = f2up;
-        this.fupl = fupl;
+  this.cmdrdc = cmdrdc;
+  this.f2up = f2up;
+  this.fupl = fupl;
 }
 
 // INPUT is in UTC, convert to browser time
-function toDate(stdt) {
+function toLocDate(stdt) {
   const MonthConversions = {"Jan": 0, "Feb": 1, "Mar": 2, "Apr": 3, "May": 4, "Jun": 5,
                             "Jul": 6, "Aug": 7, "Sep": 8, "Oct": 9, "Nov": 10, "Dec": 11};
 
@@ -30,11 +37,13 @@ function toDate(stdt) {
   minute = parseInt(stdt.substring(15, 17));
   second = parseInt(stdt.substring(18));
 
-  date = new Date(year, month, day, hour, minute, second);
+	// UTC date
+	date = new Date(Date.UTC(year, month, day, hour, minute, second));
 
-  return (date);
+	// local date
+	locDate = new Date(date.toUTCString());
 
-
+	return (locDate);
 }
 
 // get time elapsed between datapoints in hours
