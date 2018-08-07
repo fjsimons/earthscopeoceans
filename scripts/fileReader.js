7ada558bc22fdc58fc94592e97190a22d643598e
Frederik J. Simons <fjsimons@gmail.com>
2018-08-07 09:10:44 -0400

    Additional functionality and bug fixes

---
+++ scripts/fileReader.js
@@ -0,0 +1,23 @@
+/**
+	File reader class
+
+	@author Jonah Rubin
+	4/1/18
+*/
+
+
+//add callback to handle asynchronous response
+function get(url, callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", url, true);
+  xhr.onreadystatechange = function () {
+  	  // defensive check
+      if (xhr.readyState == 4) {
+          if (typeof callback === "function") {
+          	  //apply callback to response
+              callback.apply(xhr);
+          }
+      }
+  };
+  xhr.send();
+}
