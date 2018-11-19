2f7c2f44e6c75c8981f0cd7704207b71c6ca292c
Frederik J. Simons <fjsimons@gmail.com>
2018-11-19 16:04:52 -0500

    SAC headers again

--- scripts/SacFile.js
+++ scripts/SacFile.js
@@ -1,50 +1,70 @@
 /**
 	SAC file class
 	@author Jonah Rubin
-	10/29/18
+	11/19/18
 */
+
+// https://ds.iris.edu/files/sac-manual/manual.html
 
 function SacFile(sacArrayBuffer) {
 
   // split arraybuffer section by:
   // number of words * number of bits per word / number of bits per byte
 
+  // HEADER
+
   // 70 words, 32 bits per word / 8 bits per byte
-  var floats = new Float32Array(ab.slice(0,280));
-
-  // 35 words, 32 bits per word / 8 bits per byte
-  var enumerated = new Int32Array(ab.slice(280, 420));
-
+  // Floating points
+  var HdrF = new Float32Array(ab.slice(0,280));
+  // Integer
+  // 15 words, 32 bits per word / 8 bits per byte
+  var HdrN = new Int32Array(ab.slice(280, 340));
+  // Enumerated
+  // 20 words, 32 bits per word / 8 bits per byte
+  var HdrI = new Int32Array(ab.slice(340, 420));
+  // Logical
   // 5 words, 32 bits per word / 8 bits per byte
-  var logical = new Int32Array(ab.slice(420, 440));
-
+  var HdrL = new Int32Array(ab.slice(420, 440));
+  // Alphanumeric
   // 48 words, 32 bits per word / 8 bits per byte
-  var alphanum = new Int32Array(ab.slice(440, 632));
+  var HdrK = new Int32Array(ab.slice(440, 632));
 
-
+  // ACTUAL DATA
   // the rest of the file
   var data = new Float32Array(ab.slice(632, ab.length));
 
   // for debugging purposes
-  console.log("floats: ", floats);
-  console.log("enumerated: ", enumerated);
-  console.log("logical: ", logical);
-  console.log("alphanum: ", alphanum);
+  console.log("HdrF: ", HdrF);
+  console.log("HdrI: ", HdrI);
+  console.log("HdrL: ", HdrL);
+  console.log("HdrK: ", HdrK);
   console.log("data: ", data);
 
-  var deltaT = floats[0];
+  // parsed header variables
+  var DELTA = HdrF[0];  
+  var B     = HdrF[5];  
+  var STLA  = HdrF[32];
+  var STLO  = HdrF[33];
+  var STDP  = HdrF[34];
+  var NPTS  = HdrN[9];
+  var IDEP  = HdrN[15];
 
+  console.log("IDEP: ", IDEP);
+
+  // check that data.length equals NPTS
+
+  // make data pairs
   var plotData = [];
 
   // create plotting rows
-  for (var i = 0; i < 200; i++) {
+  for (var i = 0; i < data.length; i++) {
     var row = [];
-    row.push(deltaT * i);
-    row.push(data[i] / 1000);
+    row.push(B + DELTA * i);
+    row.push(data[i]);
     plotData.push(row);
-
   }
 
+  // makes the plot
   google.charts.setOnLoadCallback(drawChart(plotData));
 }
 
@@ -53,7 +73,7 @@ google.charts.load('current', {'packages':['corechart']});
 
 function drawChart(sacData) {
   var data = google.visualization.arrayToDataTable([
-    ['Time', 'Displacement'],
+    ['time', 'displacement'],
     [0,  0]
   ]);
 
@@ -65,15 +85,14 @@ function drawChart(sacData) {
     title: 'Float Name',
 
     hAxis: {
-      title: 'Time (s)'
+      title: 'time (s)'
     },
     vAxis: {
-      title: 'Displacement'
+      title: 'displacement'
     },
 
-    curveType: 'function',
-    legend: { position: 'bottom' }
-
+    curveType: 'function' //,
+    // legend: { position: 'bottom' }
 
   };
 
