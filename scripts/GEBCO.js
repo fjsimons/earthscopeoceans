e200197512130aac98ba728c8cf51be2129f6c93
Frederik J. Simons <fjsimons@gmail.com>
2019-03-12 16:17:18 -0400

    Fix in GEBCO

--- scripts/GEBCO.js
+++ scripts/GEBCO.js
@@ -1,43 +1,45 @@
 function makeWMSrequest(dataPoint) {
 
-  stlap = dataPoint.stla - .5;
-  stlop = dataPoint.stlo - .5;
-  stlam = dataPoint.stla + .5;
-  stlom = dataPoint.stlo + .5;
-  const rqtHead = 'http://www.gebco.net/data_and_products/gebco_web_services/web_map_service/mapserv?';
+    // This is inspired by the known 2014 resolution
+    const bb = 1/60/2;
 
-  // Integer width and height of the map (when requesting a feature, keep it small!)
-  const pxw = 5;
-  const pxh = 5;
+    stlap = dataPoint.stla - bb;
+    stlop = dataPoint.stlo - bb;
+    stlam = dataPoint.stla + bb;
+    stlom = dataPoint.stlo + bb;
+    const rqtHead = 'http://www.gebco.net/data_and_products/gebco_web_services/web_map_service/mapserv?';
 
-  // Integer pixel count inside the map where you want to extract the point
-  const pxx = 2;
-  const pxy = 2;
+    // Integer width and height of the map (when requesting a feature, keep it small!)
+    const pxw = 5;
+    const pxh = 5;
 
-  // get featureInfo
-  const rqt = 'getfeatureinfo';
+    // Integer pixel count inside the map where you want to extract the point
+    const pxx = 2;
+    const pxy = 2;
 
-  const rqtTail = 'request=' + rqt + '&service=wms&crs=EPSG:4326&layers=gebco_latest_2&query_layers=gebco_latest_2&BBOX='
-                  + stlap +','+ stlop + ','+ stlam + ','+ stlom + '&info_format=text/plain&service=wms&x='
-                  + pxx + '&y=' + pxy + '&width=' + pxw + '&height=' + pxh + '&version=1.3.0'
+    // get featureInfo
+    const rqt = 'getfeatureinfo';
 
-  var url = rqtHead + rqtTail;
+    const rqtTail = 'request=' + rqt + '&service=wms&crs=EPSG:4326&layers=gebco_latest_2&query_layers=gebco_latest_2&BBOX='
+	+ stlap +','+ stlop + ','+ stlam + ','+ stlom + '&info_format=text/plain&service=wms&x='
+	+ pxx + '&y=' + pxy + '&width=' + pxw + '&height=' + pxh + '&version=1.3.0'
 
-  // Use the "get" method defined in the fileReader.js
-  resp = get(url,
-  function () {
-		 // We expect a return to look like this, so you parse on the quote and get the 7th field
-		 //  GetFeatureInfo results:
-		 // Layer 'GEBCO_LATEST_2'
-		 // Feature 0: 
-		 // x = '23.2375'
-		 // y = '65.095833'
-		 // value_list = '-101'
+	var url = rqtHead + rqtTail;
 
-    console.log(this.responseText.split("\'")[7]);
-    dataPoint.wmsdepth = this.responseText.split("\'")[7];
-  });
+    // Use the "get" method defined in the fileReader.js
+    resp = get(url,
+	       function () {
+		   // We expect a return to look like this, so you parse on the quote and get the 7th field
+		   //  GetFeatureInfo results:
+		   // Layer 'GEBCO_LATEST_2'
+		   // Feature 0: 
+		   // x = '23.2375'
+		   // y = '65.095833'
+		   // value_list = '-101'
 
-  // console.log(depth);
+		   console.log(this.responseText.split("\'")[7]);
+		   dataPoint.wmsdepth = this.responseText.split("\'")[7];
+	       });
 
+    // console.log(depth);
 }
