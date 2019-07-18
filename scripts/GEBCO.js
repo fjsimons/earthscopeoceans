7d1dc9164c16d8dafd8e27ed15ea76b2e307b8ec
Frederik J. Simons <fjsimons@gmail.com>
2019-07-18 14:20:57 -0400

    Performs a WMS request for GEBCO bathymetry

--- scripts/GEBCO.js
+++ scripts/GEBCO.js
@@ -1,7 +1,6 @@
 function makeWMSrequest(dataPoint) {
-
     // This is inspired by the known 2014 resolution
-    const bb = 1/60/2;
+    const bb = 1/60/2/5;
 
     stlap = dataPoint.stla - bb;
     stlop = dataPoint.stlo - bb;
@@ -10,21 +9,21 @@ function makeWMSrequest(dataPoint) {
     const rqtHead = 'http://www.gebco.net/data_and_products/gebco_web_services/web_map_service/mapserv?';
 
     // Integer width and height of the map (when requesting a feature, keep it small!)
-    const pxw = 5;
-    const pxh = 5;
+    const pxw = 10;
+    const pxh = 10;
 
     // Integer pixel count inside the map where you want to extract the point
-    const pxx = 2;
-    const pxy = 2;
+    const pxx = 5;
+    const pxy = 5;
 
     // get featureInfo
     const rqt = 'getfeatureinfo';
 
     const rqtTail = 'request=' + rqt + '&service=wms&crs=EPSG:4326&layers=gebco_latest_2&query_layers=gebco_latest_2&BBOX='
-	+ stlap +','+ stlop + ','+ stlam + ','+ stlom + '&info_format=text/plain&service=wms&x='
+	+ stlap + ',' + stlop + ',' + stlam + ',' + stlom + '&info_format=text/plain&service=wms&x='
 	+ pxx + '&y=' + pxy + '&width=' + pxw + '&height=' + pxh + '&version=1.3.0'
 
-	var url = rqtHead + rqtTail;
+	let url = rqtHead + rqtTail;
 
     // Use the "get" method defined in the fileReader.js
     resp = get(url,
@@ -37,7 +36,7 @@ function makeWMSrequest(dataPoint) {
 		   // y = '65.095833'
 		   // value_list = '-101'
 
-		   console.log(this.responseText.split("\'")[7]);
+		   // console.log(this.responseText.split("\'")[7]);
 		   dataPoint.wmsdepth = this.responseText.split("\'")[7];
 	       });
 
