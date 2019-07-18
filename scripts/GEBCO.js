33b018759815546550f02b523a5051759f982f38
Frederik J. Simons <fjsimons@gmail.com>
2019-07-18 14:21:04 -0400

    Performs a WMS request for GEBCO bathymetry

--- scripts/GEBCO.js
+++ scripts/GEBCO.js
@@ -1,28 +1,6 @@
 function makeWMSrequest(dataPoint) {
-
-    // // This is inspired by the known 2014 resolution
-    // const bb = 1/60/2;
-    //
-    // stlap = dataPoint.stla - bb;
-    // stlop = dataPoint.stlo - bb;
-    // stlam = dataPoint.stla + bb;
-    // stlom = dataPoint.stlo + bb;
-    //
-    // const rqtHead = 'http://www.gebco.net/data_and_products/gebco_web_services/web_map_service/mapserv?';
-    //
-    // // Integer width and height of the map (when requesting a feature, keep it small!) 5,5
-    // const pxw = 5;
-    // const pxh = 5;
-    //
-    // // Integer pixel count inside the map where you want to extract the point
-    // const pxx = 2;
-    // const pxy = 2;
-
-    // New values for bb, pxw, pxh, pxx, pxy by playing around and comparing to
-    // fjsimons matlab implementation
-
     // This is inspired by the known 2014 resolution
-    const bb = 1 / 60 / 2 / 5;
+    const bb = 1/60/2/5;
 
     stlap = dataPoint.stla - bb;
     stlop = dataPoint.stlo - bb;
@@ -31,7 +9,7 @@ function makeWMSrequest(dataPoint) {
 
     const rqtHead = 'http://www.gebco.net/data_and_products/gebco_web_services/web_map_service/mapserv?';
 
-    // Integer width and height of the map (when requesting a feature, keep it small!) 5,5
+    // Integer width and height of the map (when requesting a feature, keep it small!)
     const pxw = 10;
     const pxh = 10;
 
@@ -46,10 +24,7 @@ function makeWMSrequest(dataPoint) {
         + stlap + ',' + stlop + ',' + stlam + ',' + stlom + '&info_format=text/plain&service=wms&x='
         + pxx + '&y=' + pxy + '&width=' + pxw + '&height=' + pxh + '&version=1.3.0'
 
-
     let url = rqtHead + rqtTail;
-    if (dataPoint.stdt == "10-Mar-2019 14:09:59") {
-    }
 
     // Use the "get" method defined in the fileReader.js
     resp = get(url,
@@ -62,7 +37,8 @@ function makeWMSrequest(dataPoint) {
             // y = '65.095833'
             // value_list = '-101'
 
-            dataPoint.wmsdepth = this.responseText.split("\'")[7];
+   	    // console.log(this.responseText.split("\'")[7]);
+	    dataPoint.wmsdepth = this.responseText.split("\'")[7];
         });
 
     // console.log(depth);
