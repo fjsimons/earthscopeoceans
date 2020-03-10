7cddfb8c5e7fb760065a571d2f29a839b5614a2c
Frederik J. Simons <fjsimons@gmail.com>
2020-03-10 15:41:59 -0400

    Text and binary file reader

--- scripts/fileReader.js
+++ scripts/fileReader.js
@@ -2,55 +2,24 @@
    File reader class
 
    @author Jonah Rubin
-   11/27/2019
+   03/10/2020
 */
 
-// for textfiles
-function get(url, callback) {
+// for text or binary files
+function get(type, url, callback) {
     var xhr = new XMLHttpRequest();
     xhr.open("GET", url, true);
+    if (type === "bin") {
+        xhr.responseType = 'blob';
+    }
     xhr.onreadystatechange = function () {
-	// defensive check
-	if (xhr.readyState == 4) {
-	    if (typeof callback === "function") {
-		//apply callback to response
-		callback.apply(xhr);
-	    }
-	}
+        // defensive check
+        if (xhr.readyState === 4) {
+            if (typeof callback === "function") {
+                //apply callback to response
+                callback.apply(xhr);
+            }
+        }
     };
     xhr.send();
 }
-
-// For future consolidation
-// function get(type, url, callback) {
-//     let xhr = new XMLHttpRequest();
-//     xhr.open("GET", url, true);
-//     xhr.onreadystatechange = function () {
-//         // defensive check
-//         if (xhr.readyState === 4) {
-//             if (typeof callback === "function") {
-//                 //apply callback to response
-//                 callback.apply(xhr);
-//             }
-//         }
-//     };
-//     xhr.send();
-// }
-
-// for binary files
-function getBin(url, callback) {
-    let xhr = new XMLHttpRequest();
-    xhr.open("GET", url, true);
-    xhr.responseType = 'blob';
-    xhr.onreadystatechange = function () {
-	// defensive check
-	if (xhr.readyState === 4) {
-	    if (typeof callback === "function") {
-		//apply callback to response
-		callback.apply(xhr);
-	    }
-	}
-    };
-    xhr.send();
-}
-
