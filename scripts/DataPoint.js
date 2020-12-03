4b35c5dbabbf610f960a2d38d4bd4e7fb2f7a6e0
Frederik J. Simons <fjsimons@gmail.com>
2020-12-03 18:44:29 -0500

    Defines an object class for float surfacings

--- scripts/DataPoint.js
+++ scripts/DataPoint.js
@@ -34,7 +34,7 @@ function getOwner(name) {
     if (id === 6) {
         return ("geoazur");
         // Dead MERMAIDs
-    } else if (id === 7 || id === 3) {
+     } else if (id === 7 || id === 3 || id === 34 || id === 47 ) {
         return ("dead");
         // SUSTECH MERMAIDs
     } else if (26 <= id && id <= 49) {
