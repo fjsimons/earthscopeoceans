4376704c6937ce6e4ec87d4df4f7fa316938a56e
Jonah Rubin <jonah.n.rubin@gmail.com>
2021-10-12 11:45:05 -0400

    switchign to promise instead of callback

--- scripts/fileReader.js
+++ scripts/fileReader.js
@@ -27,3 +27,21 @@ function get(type, url, callback) {
     };
     xhr.send();
 }
+
+async function fetchAndDecodeFloatData(url, type) {
+    let response = await fetch(url);
+    let content;
+  
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    } else {
+      if(type === 'blob') {
+        content = await response.blob();
+      } else if(type === 'text') {
+        content = await response.text();
+      }
+    }
+  
+    return content;
+  
+}
