21be28beced9f0e36336e835d5181f80a8353ea9
Frederik J. Simons <fjsimons@gmail.com>
2022-07-05 11:51:23 -0400

    Determines the Exclusive Economic Zone of a geographic location

--- scripts/EEZ.js
+++ scripts/EEZ.js
@@ -15,13 +15,16 @@ function pointInPolygon(point, polygon){
 	return inside
 }
 
-// Determines the Exclusive Economic Zone
+// Determines the Exclusive Economic Zone of a single latitude/longitude pair
 async function eezFinder(lat, lon, EEZList, AllGeometries){
+    // Finds the set of EEZ bounding boxes in which the point lies
     const newfilt = EEZList.filter(item => (item["minLatitude"]<=lat && item["maxLatitude"]>=lat) && (item["minLongitude"]<=lon && item["maxLongitude"]>=lon || item["minLongitude"]>item["maxLongitude"]) && item["status"]!="deleted");
     if(newfilt.length>0){
+	// Check each of the matches to see where the point really lies
 	for(let i=0;i<newfilt.length;i++){
 	    let MRGID = newfilt[i]["MRGID"];
 	    let data = AllGeometries[`${MRGID}`]
+		// Some bounding boxes have multiple disjoint polygons in them belonging to the same EEZ
 		for(let j=0;j<data.length;j++){
 		    if(pointInPolygon([lon,lat],data[j])){
 			return newfilt[i]["preferredGazetteerName"]
