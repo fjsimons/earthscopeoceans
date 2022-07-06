20c4d63bdff309049b2684145c9467b604039445
Frederik J. Simons <fjsimons@gmail.com>
2022-07-06 10:39:41 -0400

    Biographical text balloons

--- scripts/BioPopup.js
+++ scripts/BioPopup.js
@@ -1,9 +1,18 @@
 let openBios = [];
 
-// When the user clicks on div, open the popup
+// When the user clicks on div, open the popup and close other popups
 function myFunction(id) {
-    
+    for (let item of openBios){
+        if (item.id !== id){
+            item.classList.toggle("show");
+        }
+        openBios = openBios.filter(obj => obj!==item || obj.id==id);
+    }
     let popup = document.getElementById(id);
     popup.classList.toggle("show");
+    if (openBios.filter(obj => obj.id === id).length > 0){
+        openBios = openBios.filter(obj => obj.id !== id);
+        return
+    }
     openBios.push(popup);
 }
