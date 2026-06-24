4a6199f0f6368b8d9a56215532f6901a5758719e
Frederik J. Simons <fjsimons@gmail.com>
2026-06-24 14:38:26 -0400

    Performs a WMS request for GEBCO bathymetry

--- scripts/GEBCO.js
+++ scripts/GEBCO.js
@@ -1,4 +1,12 @@
 // Last modified by SKB, JNR, and FJS 6/23/26
+// 
+//      Both scripts query the GEBCO database at a chosen location
+//
+//      Here is an example of a working link:
+//          https://wms.gebco.net/mapserv?request=getfeatureinfo&crs=EPSG:4326&layers=GEBCO_LATEST_2&query_layers=GEBCO_LATEST_2&BBOX=65.0874996667,23.2291666667,65.1041663333,23.2458333333&info_format=text/plain&service=wms&i=2&j=2&width=5&height=5&version=1.3.0
+//
+//          https://wms.gebco.net/mapserv?request=getfeatureinfo&crs=EPSG:4326&layers=GEBCO_LATEST_2&query_layers=GEBCO_LATEST_2&BBOX=50.9542,-199875,50.9708,-19.9708&info_format=text/plain&service=wms&i=2&j=2&width=5&height=5&version=1.3.0
+//
 // Function to get GEBCO depth from a datapoint parameter
 function makeWMSrequest(dataPoint) {
     // This is inspired by the known 2014 resolution
@@ -24,8 +32,7 @@ function makeWMSrequest(dataPoint) {
     const rqtTail = 'request=' + rqt + '&service=wms&crs=EPSG:4326&layers=GEBCO_LATEST_2&query_layers=GEBCO_LATEST_2&BBOX='
           + stlap + ',' + stlop + ',' + stlam + ',' + stlom + '&info_format=text/plain&service=wms&i='
           + pxx + '&j=' + pxy + '&width=' + pxw + '&height=' + pxh + '&version=1.3.0';
-    // Write out one ACTUAL example that we can handcopy in the browser
-    //
+
     let url = rqtHead + rqtTail;
 
     // console.log(url);
@@ -33,17 +40,17 @@ function makeWMSrequest(dataPoint) {
     // Use the "get" method defined in the fileReader.js
     //console.log(url);
     resp = get(DataType.TEXT, url,
-	       function () {
-		   // We expect a return to look like this, so you parse on the quote and get the 7th field
-		   //  GetFeatureInfo results:
-		   // Layer 'GEBCO_LATEST_2'
-		   // Feature 0: 
-		   // x = '23.2375'
-		   // y = '65.095833'
-		   // value_list = '-101'
+        function () {
+            // We expect a return to look like this, so you parse on the quote and get the 7th field
+            //  GetFeatureInfo results:
+            // Layer 'GEBCO_LATEST_2'
+            // Feature 0: 
+            // x = '23.2375'
+            // y = '65.095833'
+            // value_list = '-101'
 
-		   dataPoint.wmsdepth = this.responseText.split("\'")[7];
-	       });
+            dataPoint.wmsdepth = this.responseText.split("\'")[7];
+        });
 
     //console.log(depth);
 }
@@ -79,3 +86,4 @@ async function makeWMSrequestCoords(lat, lng) {
     resp = await fetchAndDecodeFloatData(url, 'text')
     return resp.split("\'")[7];
 }
+
