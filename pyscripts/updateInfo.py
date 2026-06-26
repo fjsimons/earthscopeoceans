11f3438d87409a5924e6f18385087101d33c846f
Frederik J. Simons <fjsimons@gmail.com>
2026-06-26 16:22:34 -0400

    Updates traveled distance and time calculation

--- pyscripts/updateInfo.py
+++ pyscripts/updateInfo.py
@@ -4,100 +4,30 @@
 # Looks at SOM float data and runs an update to add to FloatInfo
 # 
 # Last update by Frederik J Simons on 06/10/2026
-# Last update by Stefan Kildal-Brandt on 6/24/2026
+# Last update by Stefan Kildal-Brandt on 6/25/2026
 
 import urllib2
 from urllib2 import HTTPError
-from datetime import datetime
-import math
 import os
+import re
 
-# Haversine Displacement Calculation
-def getDisplacement(datapt1, datapt2):
-    lat1 = datapt1[0]
-    lon1 = datapt1[1]
-    lat2 = datapt2[0]
-    lon2 = datapt2[1]
+from helper_functions import getDisplacement, monthToNum, toUTCTime, getGEBCODepth 
 
-    R = 6378.137
-    dLat = lat2 * math.pi / 180 - lat1 * math.pi / 180
-    dLon = lon2 * math.pi / 180 - lon1 * math.pi / 180
-    a = (math.sin(dLat/2) * math.sin(dLat/2) +
-        math.cos(lat1*math.pi/180) * math.cos(lat2*math.pi/180)*
-        math.sin(dLon/2) * math.sin(dLon/2))
-    c = 2 * math.atan2(math.sqrt(a),math.sqrt(1-a))
-    d = R * c
-    return d*1000
+# Grab the path
+path_file = open('pathNames.txt', 'r')
+path = path_file.readlines()[0].strip()
+path_file.close()
 
-# Function to convert a given month to a number
-def monthToNum(month):
-    return {
-        'Jan': 1,
-        'Feb': 2,
-        'Mar': 3,
-        'Apr': 4,
-        'May': 5,
-        'Jun': 6,
-        'Jul': 7,
-        'Aug': 8,
-        'Sep': 9,
-        'Oct': 10,
-        'Nov': 11,
-        'Dec': 12
-    }[month]
-
-# Given a date and time, get the UTC Time
-def toUTCTime(str):
-    tempArr = str.split(' ')
-    arr = tempArr[0].split('-') + tempArr[1].split(':')
-    arr[1] = monthToNum(arr[1])
-    finalArr = [int(x) for x in arr]
-    dt = datetime(finalArr[2], finalArr[1], finalArr[0], finalArr[3], finalArr[4], finalArr[5])
-    timestamp = (dt - datetime(1970, 1, 1)).total_seconds()
-    return timestamp
-
-# Grabs GEBCODepth from gebco.net given a latitude and longitude
-def getGEBCODepth(latlon, bounds=5):
-    bb = .0083333333333
-    stlap = latlon[0] - bb
-    stlop = latlon[1] - bb
-    stlam = latlon[0] + bb
-    stlom = latlon[1] + bb
-    rqtHead = 'https://wms.gebco.net/mapserv?'
-    rqtTail = ('request=getfeatureinfo&service=wms&crs=EPSG:4326&layers=GEBCO_LATEST_2&query_layers=GEBCO_LATEST_2&BBOX='
-              + str(stlap) + ',' + str(stlop) + ',' + str(stlam) + ',' + str(stlom)
-              + '&info_format=text/plain&service=wms&i=2&j=2&width=' + str(bounds) + '&height=' + str(bounds) + '&version=1.3.0')
-    url = rqtHead + rqtTail
-    try:
-        file = urllib2.urlopen(url)
-        data = str(file.read())
-        return data.split("\'")[7]
-    except HTTPError as err:
-        print(err)
-        return 0
-    except IndexError as err:
-        print(err)
-        print("Retrying with bigger bounding box")
-        if bounds<10:
-            return getGEBCODepth(latlon, bounds+1)
-        return 0
-    except Exception as err:
-        print(err)
-        return 0
-
-# List of all floats
-floats = ["N0001", "N0002", "N0003", "N0004", "N0005", "P0050", "T0100",
-          "T0101", "T0102", "P0052", "P0053", "P0054", "P0006", "P0008",
-          "P0009", "P0010", "P0011", "P0056", "P0012", "P0013", "P0016",
-          "P0017", "P0018", "P0019", "P0020", "P0021", "P0057", "P0022",
-          "P0023", "P0024", "P0025", "P0026", "P0027", "P0028", "P0029",
-          "P0031", "P0032", "P0033", "P0035", "P0036", "P0037", "P0038",
-          "P0039", "P0040", "P0041", "P0042", "P0043", "P0044", "P0045",
-          "P0046", "P0048", "P0049", "R0058", "R0059", "R0061", "R0063",
-          "R0065", "R0067", "R0069", "R0071", "R0072", "R0073", "R0001",
-          "R0002", "R0003", "R0004", "R0005", "R0007", "N0003", "P0007",
-          "P0034", "P0047", "R0006", "R0062", "R0066", "T0095", "T0096",
-          "T0097", "T0098", "T0099", "U0106", "U0107", "U0108"]
+# Grab the floats
+float_file = open('floatNames.txt', 'r')
+floats = []
+float_lines = float_file.readlines()
+for line in float_lines[1:]:
+    for flo in line.split(' '):
+        flo = flo.strip()
+        if flo:
+            floats.append(flo)
+float_file.close()
 
 # Iterate through all of the given floats
 allInfoStrings = []
@@ -105,7 +35,7 @@ for flo in floats:
     print(flo)
 
     # Grab the current data that are stored on geoweb for that float
-    floatInfoPath = '/home/www/people/simons/earthscopeoceans/data/FloatInfo/{}.txt'.format(flo)
+    floatInfoPath = os.path.join(path,'data','FloatInfo','{}.txt'.format(flo))
     if os.path.exists(floatInfoPath):
         file = open(floatInfoPath, 'r')
         fileArr = file.readlines()
@@ -178,7 +108,7 @@ for flo in floats:
         appendFile.close()
 
 # Write data in for all tabs
-with open('/home/www/people/simons/earthscopeoceans/data/FloatInfo/distances.txt', 'w') as f:
+with open(os.path.join(path,'data','FloatInfo','distances.txt'), 'w') as f:
     for line in allInfoStrings:
         f.write(line)
         if line!=allInfoStrings[-1]:
