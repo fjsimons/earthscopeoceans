1de7e019bdab9c0d3b6bb6e3dae4bb1a74f3846e
Frederik J. Simons <fjsimons@gmail.com>
2026-06-26 16:25:01 -0400

    Initialization traveled distance and time calculation

--- pyscripts/distanceCalc.py
+++ pyscripts/distanceCalc.py
@@ -1,4 +1,4 @@
-# Last modified by Stefan Kildal-Brandt 6/23/26
+# Last modified by Stefan Kildal-Brandt 6/25/26
 #
 # Turns the SOM/*_all.txt float information into
 #           FloatInfo/*.txt for every float
@@ -11,88 +11,26 @@
 
 import urllib2
 from urllib2 import HTTPError
-import math
-from datetime import datetime
+import os
+import re
 
-#Haversine Displacement Calculation
-def getDisplacement(datapt1, datapt2):
-    lat1 = datapt1[0]
-    lon1 = datapt1[1]
-    lat2 = datapt2[0]
-    lon2 = datapt2[1]
+from helper_functions import getDisplacement, monthToNum, toUTCTime, getGEBCODepth
 
-    R = 6378.137
-    dLat = lat2 * math.pi / 180 - lat1 * math.pi / 180
-    dLon = lon2 * math.pi / 180 - lon1 * math.pi / 180
-    a = (math.sin(dLat/2) * math.sin(dLat/2) +
-        math.cos(lat1*math.pi/180) * math.cos(lat2*math.pi/180)*
-        math.sin(dLon/2) * math.sin(dLon/2))
-    c = 2 * math.atan2(math.sqrt(a),math.sqrt(1-a))
-    d = R * c
-    return d*1000
+# Grab the path
+path_file = open('pathNames.txt', 'r')
+path = path_file.readlines()[0].strip()
+path_file.close()
 
-#Function to convert a given month to a number
-def monthToNum(month):
-    return {
-        'Jan': 1,
-        'Feb': 2,
-        'Mar': 3,
-        'Apr': 4,
-        'May': 5,
-        'Jun': 6,
-        'Jul': 7,
-        'Aug': 8,
-        'Sep': 9,
-        'Oct': 10,
-        'Nov': 11,
-        'Dec': 12
-    }[month]
-
-#Given a date and time, get the UTC Time
-def toUTCTime(str):
-    tempArr = str.split(' ')
-    arr = tempArr[0].split('-') + tempArr[1].split(':')
-    arr[1] = monthToNum(arr[1])
-    finalArr = [int(x) for x in arr]
-    dt = datetime(finalArr[2], finalArr[1], finalArr[0], finalArr[3], finalArr[4], finalArr[5])
-    timestamp = (dt - datetime(1970, 1, 1)).total_seconds()
-    return timestamp
-
-#Grabs GEBCODepth from gebco.net given a latitude and longitude
-def getGEBCODepth(latlon):
-    bb = .0083333333333
-    stlap = latlon[0] - bb
-    stlop = latlon[1] - bb
-    stlam = latlon[0] + bb
-    stlom = latlon[1] + bb
-    rqtHead = 'https://wms.gebco.net/mapserv?'
-    rqtTail = ('request=getfeatureinfo&service=wms&crs=EPSG:4326&layers=GEBCO_LATEST_2&query_layers=GEBCO_LATEST_2&BBOX='
-              + str(stlap) + ',' + str(stlop) + ',' + str(stlam) + ',' + str(stlom)
-              + '&info_format=text/plain&service=wms&i=2&j=2&width=5&height=5&version=1.3.0')
-    url = rqtHead + rqtTail
-    try:
-        file = urllib2.urlopen(url)
-        data = str(file.read())
-        return data.split("\'")[7]
-    except HTTPError as err:
-        print(err)
-        return 0
-    except Exception as err:
-        print(err)
-        return 0
-
-#List of all floats at time of this scripts creation
-floats = ["N0001", "N0002", "N0003", "N0004", "N0005", "P0050", "P0056",
-          "P0052", "P0053", "P0054", "P0006", "P0008", "P0009", "P0010",
-          "P0011", "P0012", "P0013", "P0016", "P0017", "P0018", "P0019",
-          "P0020", "P0021", "P0022", "P0023", "P0024", "P0025", "P0026",
-          "P0027", "P0028", "P0029", "P0031", "P0032", "P0033", "P0035",
-          "P0036", "P0037", "P0038", "P0039", "P0040", "P0041", "P0042",
-          "P0043", "P0044", "P0045", "P0046", "P0048", "P0049", "R0058",
-          "R0059", "R0061", "R0063", "R0065", "R0067", "R0069", "R0071",
-          "R0072", "R0073", "R0001", "R0002", "R0003", "R0004", "R0005",
-          "R0007", "N0003", "P0007", "P0034", "P0047", "R0006", "T0095",
-          "T0096", "T0097", "T0098", "T0099", "U0106", "U0107", "U0108"]
+# Grab the floats
+float_file = open('floatNames.txt', 'r')
+floats = []
+float_lines = float_file.readlines()
+for line in float_lines[1:]:
+    for flo in line.split(' '):
+        flo = flo.strip()
+        if flo:
+            floats.append(flo)
+float_file.close()
 
 #Script to grab and decode float data from Professor Simons' pages
 arr = []
@@ -132,7 +70,7 @@ for item in arr:
             GEBCODepth = getGEBCODepth(item[j][0])
             indStr ="{} {} {} {} {} {}".format(int(legDistance), legTime, int(getDisplacement(item[0][0], item[j][0])), int(currDistance), currTime, GEBCODepth)
             indArr.append(indStr)
-     with open('/home/www/people/simons/earthscopeoceans/data/FloatInfo/{}.txt'.format(floats[k]), 'w') as f:
+     with open(os.path.join(path,'data','FloatInfo','{}.txt'.format(floats[k])), 'w') as f:
         for line in indArr:
             f.write(line)
             if line!=indArr[-1]:
@@ -150,7 +88,7 @@ for item in arr:
     k+=1
 
 #Writes float information into a text file labelled 'distances.txt'
-with open('/home/www/people/simons/earthscopeoceans/data/FloatInfo/distances.txt', 'w') as f:
+with open(os.path.join(path,'data','FloatInfo','distances.txt'), 'w') as f:
     for line in strArr:
         f.write(line)
         if line != strArr[-1]:
