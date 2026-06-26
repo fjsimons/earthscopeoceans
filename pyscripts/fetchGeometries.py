147712da5af29d5db5a83dbdc0268cadbaf18a45
Frederik J. Simons <fjsimons@gmail.com>
2026-06-26 16:28:14 -0400

    Initialization Exclusive Economic Zones II

--- pyscripts/fetchGeometries.py
+++ pyscripts/fetchGeometries.py
@@ -2,8 +2,14 @@
 
 import json
 import urllib2
+import os
 
-with open('/home/www/people/simons/earthscopeoceans/EEZData/MRGIDList','r') as f:
+# Grab the path
+path_file = open('pathNames.txt', 'r')
+path = path_file.readlines()[0].strip()
+path_file.close()
+
+with open(os.path.join(path, 'data', 'EEZData', 'MRGIDList'), 'r') as f:
 	data = f.read()
 	f.close()
 
@@ -33,33 +39,6 @@ def convertGeo(MRGID):
 		arr.append(tempArr)
 	return arr
 
-#Old function to grab and write geometries for each EEZ separately
-#def writeFile(MRGID):
-#	convertValue = convertGeo(MRGID)
-#	arr = []
-#	for item in convertValue:
-#		newArr = []
-#		resolution = 0.1
-#		if len(item)>10000:
-#			resolution=0.5
-#		if len(item)>100000:
-#			resolution=2
-#		i=0
-#		while i<len(item)-1:
-#			k=1
-#			while abs(item[i+k][1]-item[i][1])+abs(item[i+k][0]-item[i][0])<resolution:
-#				k+=1
-#				if i+k>len(item)-2:
-#					break
-#			newArr.append([item[i][0], item[i][1]])
-#			i+=k
-#		arr.append(newArr)
-#	string = json.dumps(arr)
-#	string = string.replace(', ',',')
-#	with open('/home/www/people/simons/earthscopeoceans/data/EEZData/Geometries/{}.txt'.format(MRGID),'w') as f:
-#		f.write(string)
-#		f.close()
-
 #New function that writes all of the geometries into one file - Using this file is faster when implementing into map.js
 def writeAllFile(list):
 	obj = {}
@@ -87,7 +66,7 @@ def writeAllFile(list):
 		obj['{}'.format(MRGID)] = arr
 	string = json.dumps(obj)
 	string = string.replace(', ', ',')
-	with open('/home/www/people/simons/earthscopeoceans/data/EEZData/AllGeometries','w') as f:
+	with open(os.path.join(path,'data','EEZData','AllGeometries', 'w') as f:
 		f.write(string)
 		f.close()
 
