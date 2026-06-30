be7c6c9cb75ddf2e292c2a3b25fb0a1a374a59c0
Frederik J. Simons <fjsimons@gmail.com>
2026-06-29 22:43:16 -0400

    Performs a WMS request for GEBCO bathymetry

--- scripts/GEBCO.js
+++ scripts/GEBCO.js
@@ -7,7 +7,7 @@
 //
 //          https://wms.gebco.net/mapserv?request=getfeatureinfo&crs=EPSG:4326&layers=GEBCO_LATEST_2&query_layers=GEBCO_LATEST_2&BBOX=50.9542,-19.9875,50.9708,-19.9708&info_format=text/plain&service=wms&i=2&j=2&width=5&height=5&version=1.3.0
 //
-// Function to get GEBCO depth from a datapoint parameter
+// Function to get GEBCO elevation from a datapoint parameter
 function makeWMSrequest(dataPoint) {
     // This is inspired by the known 2014 resolution
     const bb = 1/60/2;
@@ -49,13 +49,13 @@ function makeWMSrequest(dataPoint) {
             // y = '65.095833'
             // value_list = '-101'
 
-            dataPoint.wmsdepth = this.responseText.split("\'")[7];
+            dataPoint.wmselevation = this.responseText.split("\'")[7];
         });
 
     //console.log(depth);
 }
 
-//Function to get GEBCO depth from coordinate parameters
+//Function to get GEBCO elevation from coordinate parameters
 async function makeWMSrequestCoords(lat, lng) {
     // This is inspired by the known 2014 resolution
     const bb = 1/60/2;
